8d89144ce44eebafe3480520ea9b47fc8cce33fc
Jason Haven <jason@thewebhaven.com>
2018-11-13 16:41:32 -0600

    final style tweaks and animations added

--- htdocs/assets/js/main.js
+++ htdocs/assets/js/main.js
@@ -1,1 +1,22 @@
 // Custom javascript
+$(document).ready(function(){
+
+});
+function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top - 200;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+$(window).scroll(function () {
+    $('.fade-in').each(function () {
+        if (isScrolledIntoView(this) === true && !$(this).hasClass('animated')) {
+          $(this).fadeTo(750, 1).addClass('animated');
+          console.log('gotcha');
+        }
+    });
+});
